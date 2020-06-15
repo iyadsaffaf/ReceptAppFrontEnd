9ce292b4365d8c9d048b18096846a9c9f42a9d76
Iyad Saffaf <iyadsaffaf@hotmail.com>
2020-06-15 17:42:37 +0200

    f

---
+++ src/app/Services/Data/user.service.ts
@@ -0,0 +1,20 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UserService {
+
+  constructor() { }
+}
+export class User {
+  id: string
+  name: number
+  email: string
+  createda: string
+  tags: string
+  d: string
+  url: string
+  user_id: string
+
+}
